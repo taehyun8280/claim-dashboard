9567626bab08c0713622f8ba7e522ac58179f510
wtiger988280 <wtiger988280@users.noreply.github.com>
2026-07-01 14:38:59 +0900

    Configure shared Google Sheets sync

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  googleSheetsWebAppUrl: '',
+  googleSheetsWebAppUrl: 'https://script.google.com/macros/s/AKfycbzlQs1la0yP2ne8wSBktj1A6JqSfBMb7QU6aIRWNEowCeN5WtBAHWtWb5lXEihIkhoF/exec',
   syncIntervalMs: 5000
 };
